e64fcee79d491d000a12520d37884a9ed6bef3d0
dmcswain <dmcswain@gmail.com>
2022-06-21 08:16:50 -0400

    initial commit

---
+++ src/components/Layout/Layout.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import Main from 'Main';
+import Layout, { LayoutProps } from './Layout';
+
+describe('Layout', () => {
+   const defaultProps: LayoutProps = {};
+
+   it('should render', () => {
+      const wrapper = ({ children }: { children: React.ReactNode }) => (
+         <Main>{children}</Main>
+      );
+
+      const props = { ...defaultProps };
+      const { asFragment } = render(<Layout {...props} />, { wrapper });
+
+      expect(asFragment()).toMatchSnapshot();
+      expect(screen.getByAltText('logo')).toBeTruthy();
+   });
+});
